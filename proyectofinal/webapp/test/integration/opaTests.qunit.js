f65407782927204ec95c291e1e8965905de79882
rojasrjose <joserojas.2@gmail.com>
2022-07-05 12:47:24 +0000

    Proyecto Final

---
+++ proyectofinal/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["proyectofinal/proyectofinal/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ proyectofinal/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"proyectofinal/proyectofinal/test/unit/controller/MainView.controller"
+], function () {
+	"use strict";
+});
